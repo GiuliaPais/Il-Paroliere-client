60fe99b6cc18b3141048c4bc6ef7eff42d8625c3
GiuliaPais <giuliapais1@gmail.com>
2020-06-12 14:46:38 +0200

    Commit prova 2

--- src/main/java/it/uninsubria/labB717304/clientIP/PrimaryController.java
+++ src/main/java/it/uninsubria/labB717304/clientIP/PrimaryController.java
@@ -8,6 +8,5 @@ public class PrimaryController {
     @FXML
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
-        System.out.println("hello");
     }
 }
